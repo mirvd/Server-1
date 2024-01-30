898cef0a71e659851eb2b8ff1ec854331e7aaf34
Roman Milkov <mir3227@mail.ru>
2024-01-30 23:34:51 +0500

    first commit

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server started...");
+        int port = 81;
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+            {
+                System.out.printf("New connection accepted Port: %d%n", clientSocket.getPort());
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
